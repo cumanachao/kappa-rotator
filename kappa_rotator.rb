f1c6a23937f3e4c8f885f723b95e1d79dc2a400d
Guillermo Chao <guillermo.chao@moove-it.com>
2018-01-03 18:22:07 -0300

    Fixing stuff

--- kappa_rotator.rb
+++ kappa_rotator.rb
@@ -12,12 +12,12 @@ for i in 0..fps
   kappa = MiniMagick::Image.open("#{inputKappa}")
 
   kappa.combine_options do |kappaToRotate|
-    kappaToRotate.distort('SRT', "#{i*6}")
+    kappaToRotate.distort('SRT', "#{i*(360/fps)}")
     kappaToRotate.background('transparent')
     kappaToRotate.virtual_pixel('transparent')
   end
 
-  kappa.write ("#{i}.png")
+  kappa.write("#{i}.png")
 end
 
 MiniMagick::Tool::Convert.new do |convert|
@@ -25,11 +25,9 @@ MiniMagick::Tool::Convert.new do |convert|
   convert.dispose('previous')
 
   for i in 0..fps
-    convert.delay('5')
     convert.page('+0+0', "#{i}.png")
+    convert.delay('10')
   end
-
-  convert.page('+0+0', '1.png') # Trucazo
 
   convert.loop('0')
 
